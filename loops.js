22b927eead081036d96df004f21588372f7bcc76
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-01-22 16:41:53 +0600

    Loop related problem from practice 24

---
+++ loops.js
@@ -0,0 +1,19 @@
+// practice day problem 24
+
+var budget = 1000;
+
+if(budget > 80000){
+    console.log('Ami Patti wala Mac chalai');
+}
+else if(budget > 60000){
+    console.log('Laptop dorkar');
+}
+else if(budget > 40000){
+    console.log('leneva yoga computer kinbo');
+}
+else if( budget > 20000){
+    console.log('puran Laptop kinbo');
+}
+else{
+    console.log('Gorib ar Mobile');
+}
