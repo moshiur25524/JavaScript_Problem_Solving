be55d9db1e5cecbff6356eb9c5212852f6101dff
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-01-25 13:57:03 +0600

    all the problem from module 22 is done

--- mod_22_Prob.js
+++ mod_22_Prob.js
@@ -120,3 +120,16 @@ function ticketsPrice(ticketQuantity){
 
 const PriceTotal = ticketsPrice(220)
 console.log('Total Tickets Price: ',PriceTotal);
+
+// ----------------- handle unexpected function input perameter error -----------------
+
+function add(num1 , num2){
+
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'Please enter a number'
+    }
+    return num1 + num2
+}
+
+const result = add(12, '25');
+console.log(result);
